90365049a873e3ef276c90f129e927cb7fac2cfe
Fagner Cruz <fagner@sti.ufpb.br>
2024-12-27 08:38:40 -0300

    0813 - Login Background

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,28 +1,31 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import React, { useContext } from 'react';
 
 import ApiTestes from '../../api-testes/ApiTestes';
 import LoginCreate from './LoginCreate';
 import LoginForm from './LoginForm';
 import LoginPwdLost from './LoginPwdLost';
 import LoginPwdReset from './LoginPwdReset';
-import React, { useContext } from 'react';
 import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
-  const {login} = useContext(UserContext);
+  const { login } = useContext(UserContext);
 
-  if(login === true) return <Navigate to="/conta" />
-  
+  if (login === true) return <Navigate to="/conta" />;
+
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        {/* <Route path="/" element={<ApiTestes />} /> */}
-        <Route path="create" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPwdLost />} />
-        <Route path="resetar" element={<LoginPwdReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          {/* <Route path="/" element={<ApiTestes />} /> */}
+          <Route path="create" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPwdLost />} />
+          <Route path="resetar" element={<LoginPwdReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
